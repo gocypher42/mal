0ec2b22f8fe0c319219d8484e1b2eb3716ce522e
Olivier Guenette <olivierguenette@hotmail.com>
2023-08-13 13:46:13 -0400

    Finish step0

--- impls/cpp/step0_repl.cpp
+++ impls/cpp/step0_repl.cpp
@@ -1,42 +1,40 @@
-#include "String.h"
-#include "ReadLine.h"
-
 #include <iostream>
-#include <memory>
+#include <string>
 
-String READ(const String& input);
-String EVAL(const String& ast);
-String PRINT(const String& ast);
-String rep(const String& input);
+#include "linenoise.hpp"
 
-static ReadLine s_readLine("~/.mal-history");
+using std::string;
 
-int main(int argc, char* argv[])
-{
-    String prompt = "user> ";
-    String input;
-    while (s_readLine.get(prompt, input)) {
-        std::cout << rep(input) << "\n";
+string READ(const string &input) { return input; }
+
+string EVAL(const string &input) { return input; }
+
+string PRINT(const string &input) { return input; }
+
+string rep(const string &input) {
+  string input_read = READ(input);
+  string input_eval = EVAL(input_read);
+  string input_print = PRINT(input_eval);
+  return input_print;
+}
+
+int main() {
+
+  const string history_path = "history.txt";
+  linenoise::LoadHistory(history_path.c_str());
+
+  while (true) {
+    const string prompt = "user> ";
+
+    string user_input;
+    bool quit = linenoise::Readline(prompt.c_str(), user_input);
+
+    if (quit) {
+      break;
     }
-    return 0;
-}
 
-String rep(const String& input)
-{
-    return PRINT(EVAL(READ(input)));
-}
+    std::cout << rep(user_input) << std::endl;
+  }
 
-String READ(const String& input)
-{
-    return input;
-}
-
-String EVAL(const String& ast)
-{
-    return ast;
-}
-
-String PRINT(const String& ast)
-{
-    return ast;
+  return 0;
 }
